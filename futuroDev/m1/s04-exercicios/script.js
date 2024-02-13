2be73d4b826fb59ce4feac50e48b41453bb2df4c
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-02-13 15:57:57 -0300

    m01s04 - exercício 01

---
+++ futuroDev/m1/s04-exercicios/script.js
@@ -0,0 +1,7 @@
+
+let avisos = document.getElementById('divAvisos')
+
+function executarExercicio01(){
+    avisos.innerHTML = "Hello World! <br>Não deixe de verificar o console :D"
+    console.log("Ex01: Hello World!");
+}
