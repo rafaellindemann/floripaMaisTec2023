63687fd2eb40a1a96a88ea0de19fea68876d390e
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-02-13 16:24:28 -0300

    m01s04 - exercício 02

--- futuroDev/m1/s04-exercicios/script.js
+++ futuroDev/m1/s04-exercicios/script.js
@@ -5,3 +5,13 @@ function executarExercicio01(){
     avisos.innerHTML = "Hello World! <br>Não deixe de verificar o console :D"
     console.log("Ex01: Hello World!");
 }
+function executarExercicio02(){
+    let numero = Number(prompt("Digite um número:"))
+    if(!isNaN(numero)){
+        console.log(`Ex02: Valor da variável: ${numero}`);
+        avisos.innerHTML = `Ex02: Valor da variável: ${numero}`;
+    }else{
+        console.log(`Ex02: O valor digitado não é um número`);
+        avisos.innerHTML = `Ex02: Ops, você não digitou um número...`;
+    }
+}
