d428e25325fb8d89abb760372a2dc3a8993247e0
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-02-14 21:08:25 -0300

    exercício feito durante a aula

---
+++ futuroDev/m1/exAula240214.js
@@ -0,0 +1,23 @@
+// Realizar um código para que seja perguntado ao usuário, via prompt, o nome ou código de um alimento. Após o código informado, identificar o preço deste alimento via switch e mostrá-lo ao usuário. 
+// Exemplo: morango como input e a saída mostrar R$ 5,99.
+// A execução do algoritmo deve ser contínua e o número 0 ou ‘sair’ é a flag para parar a execução.
+
+let nome = prompt(`Digite o nome do produto:\ndigite '0' para parar.`)
+
+do{
+    switch(nome.toUpperCase()){
+        case 'MORANGO': console.log(`${nome}: R$5,99 `)
+        break
+        case 'MELANCIA': console.log(`${nome}: R$15,99 `)
+        break
+        case 'LARANJA': console.log(`${nome}: R$7,99 `)
+        break
+        default: console.log(`Nome não encontrado`);
+
+    }
+    
+    
+    nome = prompt(`Digite o nome do produto:\ndigite '0' para parar.`)
+}while(nome != '0')
+
+console.log(`Programa finalizado lindamente`);
