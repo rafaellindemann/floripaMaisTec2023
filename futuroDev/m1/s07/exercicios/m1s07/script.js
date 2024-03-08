1566ffd5f87ffc0c94ce6bb2af7f58fdd35899dd
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-03-08 19:10:31 -0300

    add [M1s07] ex1

---
+++ futuroDev/m1/s07/exercicios/m1s07/script.js
@@ -0,0 +1,11 @@
+
+function selecionarRosa(){
+    document.getElementsByTagName("main")[0].style.backgroundColor = '#ff00ff'
+    // document.getElementById('conteudo').backgroundColor
+}
+function selecionarAmarelo(){
+    document.getElementsByTagName("main")[0].style.backgroundColor = '#ff0'
+}
+function selecionarVerde(){
+    document.getElementsByTagName("main")[0].style.backgroundColor = '#0f0'
+}
