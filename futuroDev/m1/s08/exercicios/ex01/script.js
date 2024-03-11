d1e2a4e07d281eaeec84ac46ee840c4764835d4a
rafaellindemann <rafaelld@gmail.com>
2024-03-11 20:24:00 -0300

    [m1s08] ex01

---
+++ futuroDev/m1/s08/exercicios/ex01/script.js
@@ -0,0 +1,10 @@
+
+function selecionarRosa(){
+    document.getElementById('conteudo').style.backgroundColor = '#ff00ff'
+}
+function selecionarAmarelo(){
+    document.getElementById('conteudo').style.backgroundColor = '#ff0'
+}
+function selecionarVerde(){
+    document.getElementById('conteudo').style.backgroundColor = '#0f0'
+}
