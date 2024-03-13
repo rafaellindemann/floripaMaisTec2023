2239314c325f91bb766ac7525e3e0cde65f6198b
rafaellindemann <rafaelld@gmail.com>
2024-03-13 20:51:10 -0300

    [m1s08] ex06

---
+++ futuroDev/m1/s08/exercicios/ex06/script.js
@@ -0,0 +1,111 @@
+[]
+function selecionarRosa(){
+    document.getElementById('conteudo').style.backgroundColor = '#ff00ff'
+}
+function selecionarAmarelo(){
+    document.getElementById('conteudo').style.backgroundColor = '#ff0'
+}
+function selecionarVerde(){
+    document.getElementById('conteudo').style.backgroundColor = '#0f0'
+}
+
+function alterarBotoes(){
+    botoes = document.getElementsByClassName('botoes');
+
+    console.log(botoes);
+
+    for(i=0; i<botoes.length; i++) {
+        if(!botoes[i].innerText.includes('Main')){
+
+            botoes[i].innerText = 'Main >> ' + botoes[i].innerText
+        }
+    }
+
+    // botoes.forEach((bt) => {
+    //     bt.innerText += 1
+    // });
+}
+
+function criarBotao(){
+    do{
+        var cor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    }while(cor.length != 7)
+    // console.log(cor.length)
+    
+    bt = document.createElement('button'); 
+    bt.setAttribute('class', 'botoes')
+    bt.setAttribute('onclick', `definirCor('${cor}')`);
+    bt.innerText = cor
+    bt.style.backgroundColor = cor
+    document.getElementById('menu').appendChild(bt);
+    
+    // console.log(cor);
+    
+    // <button class="botoes" onclick="selecionarVerde()" id="btVerde">Verde</button>
+}
+
+function definirCor(cor){
+    
+    document.getElementById('conteudo').style.backgroundColor = cor
+}
+
+function removerBotao(){
+
+    let aside = document.getElementById('menu');
+    // let ultimoFilho = aside.lastChild;
+    let ultimoFilho = aside.lastElementChild;
+
+    console.log(ultimoFilho);
+    if(ultimoFilho != null){
+        aside.removeChild(ultimoFilho);
+    }else{
+        alert("Sem filhos para remover!")
+    }
+}
+
+
+
+function alterarPorClasse(){
+    var elementos = document.querySelectorAll('.botoes, .botoes-branco');
+    console.log(elementos);
+    elementos.forEach((elemento) => {
+        if(elemento.classList.contains('botoes')){
+            elemento.classList.remove('botoes');
+            elemento.classList.add('botoes-branco');
+        } else {
+            elemento.classList.remove('botoes-branco');
+            elemento.classList.add('botoes');
+        }
+    });
+}
+
+// function alterarPorClasse() {
+//     var elementos = document.querySelectorAll('.botoes, .botoes-branco');
+
+//     elementos.forEach((elemento) => {
+//         if (elemento.style.color === 'black' || elemento.style.color === '') {
+//             elemento.style.color = 'white';
+//         } else {
+//             elemento.style.color = 'black';
+//         }
+//     });
+// }
+
+function acrescentarClasse(){
+    var elementos = document.querySelectorAll('.botoes, .botoes-branco');
+    console.log(elementos);
+
+    let n = Math.floor(Math.random()*elementos.length)
+
+    elementos[n].classList.add('destaque')
+
+    // elementos.forEach((elemento) => {
+    //     if(elemento.classList.contains('botoes')){
+    //         elemento.classList.remove('botoes');
+    //         elemento.classList.add('botoes-branco');
+    //     } else {
+    //         elemento.classList.remove('botoes-branco');
+    //         elemento.classList.add('botoes');
+    //     }
+    // });
+}
