acc4237096cebc14445eadf03dcfaf44893ccfb8
rafaellindemann <rafaelld@gmail.com>
2024-05-08 23:39:46 -0300

    Att revisão de hooks

--- futuroDev/m1/s11/rev-m1s11/src/components/HookUseEffect/index.jsx
+++ futuroDev/m1/s11/rev-m1s11/src/components/HookUseEffect/index.jsx
@@ -2,19 +2,23 @@ import { useEffect, useState } from "react"
 import './index.css'
 function HookUseEffect() {
 
+  // didMount
   // a ser executado na primeira renderização
   const [observar, setObservar] = useState(0);
   useEffect(() => {
-    console.log('useEffect primeira renderização');
+    console.log('useEffect primeira renderização porque usa somente o array vazio como segundo parâmetro');
     document.title = 'Revisão'
   }, []);
 
+  // "isto aqui é um didUpdate também, tá (Prof Dani)"
+  // didUpdate (observando uma específica)
   // a ser executado na primeira renderização e sempre que o valor do estado observar mudar
   useEffect(() => {
     console.log('useEffect: o valor do estado observar mudou, agora é', observar);
   }, [observar]);
 
-  // a ser executado em todas as renderizações
+  // didUpdate - atualizações no estado do componente (observando todas as atualizações)
+  // a ser executado em todas as renderizações porque não tem o segundo parâmetro
   useEffect(() => {
     console.log('useEffect: nova renderização (do sempre, sem parâmetro)');
   });
@@ -41,13 +45,21 @@ function HookUseEffect() {
         <p>Sempre que vamos executar algo fora do escopo React como uma chamada HTTP, a criação de um timer ou intervalo, a manipulação do DOM...</p>
         <p>
           <b>Devem estar dentro de um useEffect (porque este é o ambiente preparado para lidar com efeitos colaterais.</b>
+        </p>
+        <p>
+          <b>Ciclo de vida do componente:</b>
+          <ul>
+            <li>Nasce (didMount)</li>
+            <li>É atualizado (didUpdate)</li>
+            <li>Vai morrer (willUnmount)</li>
+          </ul>
+        </p>
           <p>Exemplos perdidos</p>
           <ul>
             <li>document.title = "Revisão"</li>
             <li>fetch()</li>
             <li>serInterval()</li>
           </ul>
-        </p>
     </div>
   )
 }
