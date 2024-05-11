42a45dd66b76b5aa8da0da2d13f14fa0280e078c
rafaellindemann <rafaelld@gmail.com>
2024-05-11 19:50:26 -0300

    att react router dom

--- futuroDev/m1/s12/m1s12/src/main.jsx
+++ futuroDev/m1/s12/m1s12/src/main.jsx
@@ -2,16 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter} from 'react-router-dom'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+import Home from './pages/Home.jsx'
+import Cadastro from './pages/Cadastro.jsx'
+import Lista from './pages/Lista.jsx'
 
 const router = createBrowserRouter([
   {
-    
-  }
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/cadastro',
+    element: <Cadastro />,
+  },
+  {
+    path: '/lista',
+    element: <Lista />,
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <RouterProvider router={router}>
+
+  </RouterProvider>
 )
---
+++ futuroDev/m1/s12/repoDasAulas/semana12/src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import {Outlet} from "react-router-dom"
+import {UsuariosContextProvider} from "./context/UsuariosContext"
+
+function App(){
+  return (
+    <UsuariosContextProvider>
+      <Navbar />
+      <Outlet />
+      <footer>
+        Created by Bruno Costa
+      </footer>
+    </UsuariosContextProvider>
+  )
+}
+
+export default App;
