003075b0cf9454a95c1284971d58498bb46e4580
Professor SESI/SENAI <Professor@SN-371157.edu.fiesc.com.br>
2024-07-10 18:10:23 -0300

    Add async readline wip

---
+++ futuroDev/m2/s03/240527/src/index.js
@@ -0,0 +1,17 @@
+const readline = require('readline/promises')
+
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+async function iniciar(){
+    const altura = await input.question("Altura do Triângulo: ")
+    const base = await input.question("Base do Triângulo: ")
+
+    console.log
+
+
+}
+
+iniciar()
