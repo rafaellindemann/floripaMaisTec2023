5d0460473d1c6b8ed1045afc2beef92dc897d238
rafaellindemann <rafaelld@gmail.com>
2024-07-18 01:14:26 -0300

    add rota /vacinas

--- futuroDev/m2/s05/a01/src/index.js
+++ futuroDev/m2/s05/a01/src/index.js
@@ -46,6 +46,31 @@ app.post('/pets', async (req, res) => {
     // res.send('Pet cadastrado com sucesso!');
 });
 
+app.post('/vacinas', async (req, res) => {
+
+  try {
+    const dados = req.body;
+    console.log(dados);
+  
+    if(!dados.nome || !dados.descricao || !dados.dose) {
+        res.status(400).json({mensagem: 'Dados inválidos'});
+        return;
+    }
+    
+    await conexao.query(
+      `
+        INSERT INTO vacinas (nome, descricao, dose) 
+        VALUES ($1, $2, $3)
+      `, [dados.nome, dados.descricao, dados.dose]);
+  
+    res.status(201).json({mensagem: 'Vacina cadastrada com sucesso!'});
+    
+  } catch {
+    res.status(500).json({mensagem: 'Não foi possível cadastrar a vacina'});
+  }
+
+});
+
 app.get('/bemvindo', (req, res) => {
   res.send('Bem vindo então!');
 });
