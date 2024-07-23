385f90b1367b0bf31d2852ec05a73733892779bc
rafaellindemann <rafaelld@gmail.com>
2024-07-23 08:56:22 -0300

    add pet update no controller

--- futuroDev/m2/s05/a01/src/controllers/PetController.js
+++ futuroDev/m2/s05/a01/src/controllers/PetController.js
@@ -1,6 +1,12 @@
+const {Pool} = require('pg');
+const conexao = new Pool({ // cria a conexão com o banco de dados
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'postgre',
+  database: 'api_pets'
+})
 class PetController{
-
-  //TODO: post/criar tá pronto, falta trazer os outros métodos pra cá
 
     async criar(req, res){
   
@@ -26,6 +32,79 @@ class PetController{
         }
       
       }
+
+    async listar(req, res){
+      const dados = req.query;
+  
+      if(dados.nome) {
+        try {
+          const pets = await conexao.query('SELECT * FROM pets where nome ilike $1', [`%${dados.nome}%`]);
+          res.status(200).json(pets.rows);
+        } catch (err) {
+          console.error(err);
+          res.status(500).send('Erro ao consultar a tabela');
+        }
+      }else {
+        try {
+          const pets = await conexao.query('SELECT * FROM pets');
+          res.status(200).json(pets.rows);
+        } catch (err) {
+          console.error(err);
+          res.status(500).send('Erro ao consultar a tabela');
+        }
+      }
+    }
+
+    //TODO: se passar id que não existe ainda está retornando 204
+    async deletar(req, res){
+      const id = req.params.id;
+    
+      try {
+        await conexao.query('DELETE FROM pets WHERE id = $1', [id]);
+        res.status(204).json();
+      } catch {
+        res.status(500).json({mensagem: 'Não foi possível excluir o pet'});
+      }
+    }
+
+    async atualizar(req, res){
+      try {
+          const dados = req.body
+          const id = req.params.id
+
+          const dadosDoPet = await conexao.query(`
+                  select * from pets
+                  where id = $1
+              `, [id])
+
+          const petAtualizado = await conexao.query(`
+            update pets set 
+            nome = $1,
+            idade = $2,
+            raca = $3,
+            tipo = $4,
+            responsavel = $5
+            
+            where id = $6
+            returning *
+          `, [
+              dados.nome || dadosDoPet.rows[0].nome,
+              dados.idade || dadosDoPet.rows[0].idade,
+              dados.raca || dadosDoPet.rows[0].raca,
+              dados.tipo || dadosDoPet.rows[0].tipo,
+              dados.responsavel || dadosDoPet.rows[0].responsavel,
+              id
+          ])
+
+          res.json(petAtualizado.rows[0])
+
+      } catch (error) {
+          console.log(error)
+          res.status(500).json({
+              mensagem: 'Houve um erro ao atualizar o pet'
+          })
+      }
+  }
   
     // from copilot
     // constructor(petService){
--- futuroDev/m2/s05/a01/src/routes/pets.routes.js
+++ futuroDev/m2/s05/a01/src/routes/pets.routes.js
@@ -14,45 +14,9 @@ const conexao = new Pool({ // cria a conexão com o banco de dados
 const PetController = require('../controllers/PetController');
 
 
-petsRoutes.get("/", async (req, res) => {
-    const dados = req.query;
-
-    if(dados.nome) {
-      try {
-        const pets = await conexao.query('SELECT * FROM pets where nome ilike $1', [`%${dados.nome}%`]);
-        res.status(200).json(pets.rows);
-      } catch (err) {
-        console.error(err);
-        res.status(500).send('Erro ao consultar a tabela');
-      }
-    }else {
-      try {
-        const pets = await conexao.query('SELECT * FROM pets');
-        res.status(200).json(pets.rows);
-      } catch (err) {
-        console.error(err);
-        res.status(500).send('Erro ao consultar a tabela');
-      }
-    }
-  
-  });
-  
-  
-  // ex: localhost:3000/pets/6
-  
-  //TODO: se passar id que não existe ainda está retornando 204
-  petsRoutes.delete('/:id', async (req, res) => {
-    const id = req.params.id;
-  
-    try {
-      await conexao.query('DELETE FROM pets WHERE id = $1', [id]);
-      res.status(204).json();
-    } catch {
-      res.status(500).json({mensagem: 'Não foi possível excluir o pet'});
-    }
-  
-  });
-
+petsRoutes.get("/", PetController.listar);
+petsRoutes.delete('/:id', PetController.deletar);  
 petsRoutes.post('/', PetController.criar );
+petsRoutes.put('/:id', PetController.atualizar);
 
-  module.exports = petsRoutes;
+module.exports = petsRoutes;
