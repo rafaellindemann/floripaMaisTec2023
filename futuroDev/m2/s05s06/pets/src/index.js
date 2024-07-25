2354f0a77a3090afdf2226f11ef844196621620e
rafaellindemann <rafaelld@gmail.com>
2024-07-25 01:47:19 -0300

    add rota de serviços

--- futuroDev/m2/s05s06/pets/src/index.js
+++ futuroDev/m2/s05s06/pets/src/index.js
@@ -3,12 +3,16 @@ const express = require('express');
 
 const petsRoutes = require('./routes/pets.routes');
 const vacinasRoutes = require('./routes/vacinas.routes');
+const servicosRoutes = require('./routes/servicos.routes');
 
 const app = express();
 app.use(express.json()); // habilita o server a receber json
 // app.use(petRoutes); // manda o app usar o petRoutes que foi importado para gerenciar as rotas
 app.use('/pets', petsRoutes); // manda o app usar o petRoutes que foi importado para gerenciar as rotas e define o prefixo /pets para as rotas do petRoutes
 app.use('/vacinas', vacinasRoutes); // manda o app usar o vacinaRoutes que foi importado para gerenciar as rotas
+app.use('/servicos', servicosRoutes); // manda o app usar o servicosRoutes que foi importado para gerenciar as rotas
+
+
 
 
 
---
+++ futuroDev/m2/s05s06/pets/src/routes/servicos.routes.js
@@ -0,0 +1,43 @@
+const {Router} = require('express');
+const {Pool} = require('pg');
+
+const conexao = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgre',
+    database: 'api_pets'
+})
+
+
+const servicosRoutes = new Router();
+
+servicosRoutes.post('/', async (req, res) => {
+
+    try {
+      const dados = req.body;
+      console.log(dados);
+    
+      if(!dados.nome ||  !dados.preco && dados.preco !== 0) {
+          res.status(400).json({mensagem: 'Dados inválidos'});
+          return;
+      }
+      
+      const servico = await conexao.query(
+        `
+          INSERT INTO servicos 
+            (nome, descricao, preco) 
+            VALUES ($1, $2, $3)
+            RETURNING *
+        `, [dados.nome, dados.descricao, dados.preco]);
+    
+    //   res.status(201).json({mensagem: 'Serviço cadastrado com sucesso!'});
+      res.status(201).json(servico.rows[0]); // retorna o serviço cadastrado, o 'RETURNING *' faz com que o insert retorne o registro inserido
+      
+    } catch {
+      res.status(500).json({mensagem: 'Não foi possível cadastrar o serviço'});
+    }
+  
+  });
+
+  module.exports = servicosRoutes;
