87b727fad41a50d44840d36e386578daf9feba3c
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-07-25 19:10:56 -0300

    add get servicos

--- futuroDev/m2/s05s06/pets/src/routes/servicos.routes.js
+++ futuroDev/m2/s05s06/pets/src/routes/servicos.routes.js
@@ -1,43 +1,11 @@
 const {Router} = require('express');
-const {Pool} = require('pg');
+const ServicoController = require('../controllers/ServicoController');
 
-const conexao = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgre',
-    database: 'api_pets'
-})
 
 
 const servicosRoutes = new Router();
 
-servicosRoutes.post('/', async (req, res) => {
-
-    try {
-      const dados = req.body;
-      console.log(dados);
-    
-      if(!dados.nome ||  !dados.preco && dados.preco !== 0) { // testa os não falsy e aceita preço 0
-          res.status(400).json({mensagem: 'Dados inválidos'});
-          return;
-      }
-      
-      const servico = await conexao.query(
-        `
-          INSERT INTO servicos 
-            (nome, descricao, preco) 
-            VALUES ($1, $2, $3)
-            RETURNING *
-        `, [dados.nome, dados.descricao, dados.preco]);
-    
-    //   res.status(201).json({mensagem: 'Serviço cadastrado com sucesso!'});
-      res.status(201).json(servico.rows[0]); // retorna o serviço cadastrado, o 'RETURNING *' faz com que o insert retorne o registro inserido
-      
-    } catch {
-      res.status(500).json({mensagem: 'Não foi possível cadastrar o serviço'});
-    }
-  
-  });
+servicosRoutes.post('/', ServicoController.criar);
+servicosRoutes.get('/', ServicoController.listarTodos);
 
   module.exports = servicosRoutes;
