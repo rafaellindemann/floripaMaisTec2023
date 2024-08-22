e0fb0bd88b04bc065d1c25f600add728e635f732
rafaellindemann <rafaelld@gmail.com>
2024-08-22 01:17:44 -0300

    refatorando server

--- futuroDev/m2/s07/pets/src/routes/routes.js
+++ futuroDev/m2/s07/pets/src/routes/routes.js
@@ -1,7 +1,13 @@
-const petsRoutes = require('./routes/pets.routes');
-const vacinasRoutes = require('./routes/vacinas.routes');
-const servicosRoutes = require('./routes/servicos.routes');
+const { Router } = require('express');                  // importa o módulo express
 
-app.use('/pets', petsRoutes);               // manda o app usar o petRoutes que foi importado para gerenciar as rotas e define o prefixo /pets para as rotas do petRoutes
-app.use('/vacinas', vacinasRoutes);         // manda o app usar o vacinaRoutes que foi importado para gerenciar as rotas
-app.use('/servicos', servicosRoutes);       // manda o app usar o servicosRoutes que foi importado para gerenciar as rotas
+const petsRoutes = require('./pets.routes');            // importa o módulo pets.routes.js
+const vacinasRoutes = require('./vacinas.routes');
+const servicosRoutes = require('./servicos.routes');
+
+const routes = new Router();                                // cria uma instância de Router
+
+routes.use('/pets', petsRoutes);                           // manda o routes usar o petRoutes que foi importado para gerenciar as rotas e define o prefixo /pets para as rotas do petRoutes
+routes.use('/vacinas', vacinasRoutes);                     // manda o routes usar o vacinaRoutes que foi importado para gerenciar as rotas
+routes.use('/servicos', servicosRoutes);                   // manda o routes usar o servicosRoutes que foi importado para gerenciar as rotas
+
+module.exports = routes;                                   // exporta o routes para ser usado em outros módulos
